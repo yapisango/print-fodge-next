afec84c6cca76ec9131902de7d1c155bddc74890
unknown <yapisango@gmail.com>
2025-07-08 15:41:45 +0200

    Set up initial routes and page builds for PrintForge layout and About sections
    - Generated initial application build for PrintForge using Next.js.
    - Added layout and main app structure with bundled styles and scripts:
      - /layout includes layout.js and global styles (layout.css)
    - Configured the main page (/page) with associated JS chunks.
    - Added routing for the About section:
      - /about/page
      - /about/mission/page
    - Each route is compiled with shared webpack and main app chunks, ensuring optimized reuse.
    - This establishes the foundational routing and static asset bundling structure for the app.

---
+++ app/about/mission/page.jsx
@@ -0,0 +1,5 @@
+export default function MissionPage () {
+    return (
+        <h1>This is our mission statement page!</h1>
+    )
+}
--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,19 @@
-import "./globals.css";
+import "./globals.css"
+import Link from "next/link"
+import { Albert_Sans, Montserrat_Alternates } from "next/font/google"
+
+const albertSans = Albert_Sans({
+  subsets: ["latin"],
+  display: "swap",
+  variable: "--font-albert-sans",
+})
+
+const montserratAlternates = Montserrat_Alternates({
+  subsets: ["latin"],
+  display: "swap",
+  weight: ["100", "200", "300", "400", "500", "600", "700", "800", "900"],
+  variable: "--font-montserrat-alternates"
+})
 
 export default function RootLayout({
   children,
@@ -6,8 +21,34 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
+    <html lang="en"
+        className={`${montserratAlternates.variable} ${albertSans.variable}`}
+      >
       <body>
+        <header className="w-full bg-white">
+          <nav className="flex justify-between px-6 py-4">
+            <Link href="/">
+            <div className="flex items-center">
+              {/* Desktop logo */}
+              <img
+                src="images/printforge-logo.svg"
+                alt="PrintForge Logo"
+                className="w-[200px] h-auto hidden md:block object-contain"
+              />
+              {/* Mobile logo */}
+              <img
+                src="images/printforge-logo-icon.svg"
+                alt="PrintForge Logo"
+                className="w-[40px] h-auto block md:hidden object-contain"
+              />
+            </div>
+            </Link>
+            <ul className="flex items-center gap-2.5">
+              <li><Link href="/3d-models">3D Models</Link></li>
+              <li><Link href="/about">AboutPage</Link></li>
+            </ul>
+          </nav>
+        </header>
         {children}
       </body>
     </html>
