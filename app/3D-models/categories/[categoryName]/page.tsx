af9db951062bf37f9a918cb6e89dbb33081c7ec0
unknown <yapisango@gmail.com>
2025-07-12 20:09:25 +0200

    chore: normalize line endings and ignore node_modules

--- app/3D-models/categories/[categoryName]/page.tsx
+++ app/3D-models/categories/[categoryName]/page.tsx
@@ -1,7 +1,7 @@
 import ModelsGrid from "@/app/components/ModelsGrid"
 import { getCategoryBySlug } from "@/app/lib/categories"
 import { getModels } from "@/app/lib/models"
-import type { CategoryPageProps } from "@/types"
+import type { CategoryPageProps } from "@/app/types"
 
 export default async function CategoryPage({ params }: CategoryPageProps) {
   const { categoryName } = await params
